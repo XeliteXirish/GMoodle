96ea4a7bce0a060d9c49e786f9c75c8f40902b3c
Shaun O'Neill <shaunoneill7@me.com>
2018-09-06 00:53:45 +0100

    Push config file

---
+++ ex_config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    databaseUrl: 'mongodb://',
+    port: '',
+    client_id: '',
+    client_secret: '',
+    callback_url: 'http://localhost:<PORT>/auth/google/callback',
+    session_secret: '',
+    calenderName: '',
+    debug: false
+};
